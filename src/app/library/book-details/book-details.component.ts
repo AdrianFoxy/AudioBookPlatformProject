059a6ec6b1c36f7550c957683b9f6b713b846dd1
AdrianFoxy <drugakadrian@gmail.com>
2023-08-26 09:16:54 +0300

    AudioPlayer_Aplha_0.01

--- src/app/library/book-details/book-details.component.ts
+++ src/app/library/book-details/book-details.component.ts
@@ -6,5 +6,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./book-details.component.scss']
 })
 export class BookDetailsComponent {
+  song = new Audio();
+  songs = [
+    {url:'/assets/audio/FlyMeToTheMoon.mp3', name: 'Fly Me To The Moon'},
+    {url:'/assets/audio/LemonTree.mp3', name: 'Lemon Tree'}
+  ]
+
+  currentTime = '0:00';
+  duration = '0:00';
+  currentPercentage = 0;
+
+  openSong(song:any){
+    this.song.src = song;
+    this.song.load();
+    this.song.play();
+    this.song.addEventListener('loadedmetadata', () => {
+      const minutes = Math.floor(this.song.duration / 60);
+      const seconds = Math.floor(this.song.duration % 60);
+      this.duration = `${minutes}:${String(seconds).padStart(2, '0')}`;
+    });
+    this.song.addEventListener('timeupdate', () => {
+      const minutes = Math.floor(this.song.currentTime / 60);
+      const seconds = Math.floor(this.song.currentTime % 60);
+      this.currentTime = `${minutes}:${String(seconds).padStart(2, '0')}`;
+      this.currentPercentage = (this.song.currentTime / this.song.duration) * 100;
+    });
+  }
+
+  playSong(){
+    this.song.play();
+  }
+
+  pauseSong(){
+    this.song.pause();
+  }
+
+  stopSong(){
+    this.song.pause();
+    this.song.currentTime = 0;
+  }
+
+  vol(vl:any){
+    this.song.volume=vl.target.value;
+  }
+
+  seekSong(event: MouseEvent) {
+    const progressBar = event.target as HTMLProgressElement;
+    const clickPosition = event.offsetX;
+    const progressBarWidth = progressBar.offsetWidth;
+    const seekTime = (clickPosition / progressBarWidth) * this.song.duration;
+  }
 
 }
