033d827b308441c9f188437ae1239f75e996a997
AdrianFoxy <drugakadrian@gmail.com>
2023-09-01 09:45:33 +0300

    AudioPlayer_Aplha_0.02

--- src/app/library/book-details/book-details.component.ts
+++ src/app/library/book-details/book-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-book-details',
@@ -6,55 +8,102 @@ import { Component } from '@angular/core';
   styleUrls: ['./book-details.component.scss']
 })
 export class BookDetailsComponent {
-  song = new Audio();
-  songs = [
+  audio = new Audio();
+  audioList = [
     {url:'/assets/audio/FlyMeToTheMoon.mp3', name: 'Fly Me To The Moon'},
     {url:'/assets/audio/LemonTree.mp3', name: 'Lemon Tree'}
   ]
 
-  currentTime = '0:00';
-  duration = '0:00';
-  currentPercentage = 0;
+  audioEvents = [
+    "ended",
+    "error",
+    "play",
+    "pause",
+    "timeupdate",
+    "canplay",
+    "loadedmetadata",
+    "loadstart"
+  ];
 
-  openSong(song:any){
-    this.song.src = song;
-    this.song.load();
-    this.song.play();
-    this.song.addEventListener('loadedmetadata', () => {
-      const minutes = Math.floor(this.song.duration / 60);
-      const seconds = Math.floor(this.song.duration % 60);
-      this.duration = `${minutes}:${String(seconds).padStart(2, '0')}`;
+  currentTime = '00:00:00';
+  duration = '00:00:00';
+  seek = 0;
+
+
+  openAudioFile(audio:any){
+    this.streamObserver(audio).subscribe(event => {});
+
+    console.log(audio);
+  }
+
+  playAudio(){
+    this.audio.play();
+    console.log("Clicked play btn");
+  }
+
+  pauseAudio(){
+    this.audio.pause();
+    console.log("Clicked pause btn");
+  }
+
+  stopAudio(){
+    this.audio.pause();
+    this.audio.currentTime = 0;
+    console.log("Clicked stop btn");
+  }
+
+  setVolume(ev:any){
+    this.audio.volume = ev.target.value;
+    console.log(ev.target.value);
+  }
+
+  formatTime(time: number, format: string = "HH:mm:ss") {
+    const momentTime = time * 1000;
+    return moment.utc(momentTime).format(format);
+  }
+
+  setSeekTo(ev: any){
+    this.audio.currentTime = ev.target.value;
+  }
+
+  streamObserver(audio:any){
+    return new Observable(observer => {
+
+      this.audio.src = audio;
+      this.audio.load();
+      this.audio.play();
+
+      const handler = (event: Event) => {
+        console.log(event);
+        this.seek = this.audio.currentTime;
+        this.duration = this.formatTime(this.audio.duration);
+        this.currentTime = this.formatTime(this.audio.currentTime);
+      }
+
+      this.addEvent(this.audio, this.audioEvents, handler);
+
+      return () => {
+        this.audio.pause();
+        this.audio.currentTime = 0;
+
+        this.removeEvent(this.audio, this.audioEvents, handler);
+      }
     });
-    this.song.addEventListener('timeupdate', () => {
-      const minutes = Math.floor(this.song.currentTime / 60);
-      const seconds = Math.floor(this.song.currentTime % 60);
-      this.currentTime = `${minutes}:${String(seconds).padStart(2, '0')}`;
-      this.currentPercentage = (this.song.currentTime / this.song.duration) * 100;
+  }
+
+  addEvent(obj: EventTarget, events: string[], handler: (event: Event) => void) {
+    events.forEach(event => {
+      obj.addEventListener(event, handler);
     });
   }
 
-  playSong(){
-    this.song.play();
+  removeEvent(obj: EventTarget, events: string[], handler: (event: Event) => void) {
+    events.forEach(event => {
+      obj.removeEventListener(event, handler);
+    });
   }
 
-  pauseSong(){
-    this.song.pause();
-  }
 
-  stopSong(){
-    this.song.pause();
-    this.song.currentTime = 0;
-  }
 
-  vol(vl:any){
-    this.song.volume=vl.target.value;
-  }
-
-  seekSong(event: MouseEvent) {
-    const progressBar = event.target as HTMLProgressElement;
-    const clickPosition = event.offsetX;
-    const progressBarWidth = progressBar.offsetWidth;
-    const seekTime = (clickPosition / progressBarWidth) * this.song.duration;
-  }
 
 }
