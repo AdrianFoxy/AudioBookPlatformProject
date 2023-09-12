317311bf80382d2f24d64dc597c7652968994032
AdrianFoxy <drugakadrian@gmail.com>
2023-09-12 10:44:41 +0300

    recomendation component + resposive img for book details

---
+++ src/app/shared/recomentdations/recomentdations.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { Book } from '../models/book';
+
+@Component({
+  selector: 'app-recomentdations',
+  templateUrl: './recomentdations.component.html',
+  styleUrls: ['./recomentdations.component.scss']
+})
+export class RecomentdationsComponent {
+  books: Book[] = [
+    {
+      id: 2,
+      title: 'Ім\'я вітру',
+      author: 'Патрік Ротфусс',
+      imageSrc: '/assets/images/books_templates/TheNameOfWind.jpg',
+      rating: 4.5
+    },
+    {
+      id: 3,
+      title: 'Гоббіт',
+      author: 'Джон Толкін',
+      imageSrc: '/assets/images/books_templates/hobbit.jpg',
+      rating: 4.2
+    },
+    {
+      id: 100,
+      title: 'Морт',
+      author: 'Тіррі Пратчетт',
+      imageSrc: '/assets/images/books_templates/mort.jpg',
+      rating: 4.7
+    },
+    {
+      id: 200,
+      title: 'Хроніки Нарнії',
+      author: 'К.С. Льюїс',
+      imageSrc: '/assets/images/books_templates/narnia.jpg',
+      rating: 4.9
+    }
+  ];
+}
