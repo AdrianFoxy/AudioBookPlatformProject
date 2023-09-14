1733c09957b85cc0d81ec0cf3e0ee197d1cc2bf4
AdrianFoxy <drugakadrian@gmail.com>
2023-09-14 10:16:06 +0300

    i18n added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeModule } from './home/home.module';
 import { CoreModule } from './core/core.module';
 import { LibraryModule } from './library/library.module';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+
+
+export function HttpLoaderFactory(http:HttpClient){
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -18,7 +26,15 @@ import { LibraryModule } from './library/library.module';
     BrowserAnimationsModule,
     CoreModule,
     HomeModule,
-    LibraryModule
+    LibraryModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps:[HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
