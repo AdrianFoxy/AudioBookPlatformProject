b02084bbb65400938c329a474cc4cad87e0f4cad
AdrianFoxy <drugakadrian@gmail.com>
2023-09-27 07:37:45 +0300

    nav-bar-modal updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent{
   title = 'ABP_Client';
-  authors: any[] = [];
-
-  constructor(private http: HttpClient){
-
-  }
-  ngOnInit(): void {
-    // this.http.get('https://localhost:7088/api/AudioBook/get-authors').subscribe({
-    //   next: (response: any) => console.log(response),
-    //   error: error => console.log(error),
-    //   complete: () => {
-    //     console.log('request completed');
-    //     console.log('extra statment');
-    //   }
-    // })
-   }
 }
