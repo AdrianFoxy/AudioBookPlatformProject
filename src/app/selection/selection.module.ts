5f92979a17c06a8bb3bb9df325573e6d264c35a2
AdrianFoxy <drugakadrian@gmail.com>
2023-10-11 14:35:13 +0300

    components

---
+++ src/app/selection/selection.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SelectionComponent } from './selection.component';
+
+
+
+@NgModule({
+  declarations: [
+    SelectionComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SelectionComponent
+  ]
+})
+export class SelectionModule { }
