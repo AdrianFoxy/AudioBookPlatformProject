a852d9b14da07d2e5ba62b09fb01fd87a366d29f
AdrianFoxy <drugakadrian@gmail.com>
2023-10-19 09:42:55 +0300

    LoginPage

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DarkModeService } from 'src/app/core/services/dark-mode-service/dark-mode.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  constructor(public darkmodeService: DarkModeService) {
+  }
 }
