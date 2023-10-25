d0dc98d528b5c729d54aae31f1edd4c34a3547bf
AdrianFoxy <drugakadrian@gmail.com>
2023-10-25 07:57:36 +0300

    auth and anon guards

--- src/app/account/account-routing.module.ts
+++ src/app/account/account-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
+import { anonGuard } from '../core/guards/anon.guard';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'login', component: LoginComponent, canActivate: [anonGuard]},
+  {path: 'register', component: RegisterComponent, canActivate: [anonGuard]}
 ]
 
 @NgModule({
