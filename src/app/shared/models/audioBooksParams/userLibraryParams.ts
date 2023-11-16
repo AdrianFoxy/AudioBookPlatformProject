53a1d648f19ec16a5e626c984f09aa38a7e68bda
AdrianFoxy <drugakadrian@gmail.com>
2023-11-16 08:34:28 +0200

    User-profile, user-library

---
+++ src/app/shared/models/audioBooksParams/userLibraryParams.ts
@@ -0,0 +1,6 @@
+export class userLibraryParams {
+  pageNumber = 1;
+  pageSize = 10;
+  userId = 0;
+  statusId = 0;
+}
--- src/app/user-profile/user-profile.service.ts
+++ src/app/user-profile/user-profile.service.ts
@@ -1,10 +1,13 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { User } from '../shared/models/user';
 import { AccountService } from '../account/account.service';
 import { switchMap } from 'rxjs';
+import { userLibraryParams } from '../shared/models/audioBooksParams/userLibraryParams';
+import { AudioBook } from '../shared/models/audiobook';
+import { Pagination } from '../shared/models/pagination';
 
 @Injectable({
   providedIn: 'root'
@@ -26,5 +29,17 @@ export class UserProfileService {
   getUser(username: string){
     const header = new HttpHeaders().set('Content-type', 'application/json');
     return this.http.get<User>(this.baseUrl + 'UserProfile/' + username, { headers: header, withCredentials: true });
+  }
+
+  getUserLibrary(userLibraryParams: userLibraryParams){
+    const header = new HttpHeaders().set('Content-type', 'application/json');
+    let params = new HttpParams();
+    params = params.append('UserId', userLibraryParams.userId);
+    params = params.append('StatusId', userLibraryParams.statusId);
+    params = params.append('PageIndex', userLibraryParams.pageNumber);
+    params = params.append('PageSize', userLibraryParams.pageSize);
+
+    return this.http.get<Pagination<AudioBook[]>>(this.baseUrl + 'UserLibrary?' + params,
+    { headers: header, withCredentials: true });
   }
 }
