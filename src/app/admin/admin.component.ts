efbfd9adeb4de34a175221400029f61f02fa4bf3
AdrianFoxy <drugakadrian@gmail.com>
2024-01-08 09:36:17 +0200

    chart-data-service

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AdminService } from './admin.service';
+import { AccountService } from '../account/account.service';
 
 @Component({
   selector: 'app-admin',
@@ -7,16 +9,66 @@ import { Component } from '@angular/core';
 })
 export class AdminComponent {
 
-  barChartData = {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-    datasets: [
-      {
-        data: [89, 34, 43, 54, 28, 74, 93, 43, 54, 28, 74, 2],
-        label: 'Number of new users',
-        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-        borderColor: 'rgb(75, 192, 192)',
-        borderWidth: 1
+  userCount: number = 0;
+  reviewCount: number = 0;
+  audioBookCount: number = 0;
+  newUsersCount: number[] = [];
+  barChartData: any;
+
+  constructor(private adminService: AdminService, public accountService: AccountService) { }
+
+  ngOnInit() {
+    this.loadUserCount();
+  }
+
+  loadUserCount() {
+    this.adminService.getReviewCount().subscribe({
+      next: (reviewCount: number) => {
+        this.reviewCount = reviewCount;
+      },
+      error: (error) => {
+        console.error('Error loading review count', error);
       }
-    ]
+    });
+
+    this.adminService.getAudioBookCount().subscribe({
+      next: (audioBookCount: number) => {
+        this.audioBookCount = audioBookCount;
+      },
+      error: (error) => {
+        console.error('Error loading audiobook count', error);
+      }
+    });
+
+    this.adminService.getUserCount().subscribe({
+      next: (userCount: number) => {
+        this.userCount = userCount;
+      },
+      error: (error) => {
+        console.error('Error loading user count', error);
+      }
+    });
+
+    this.adminService.getNewUsersCount().subscribe({
+      next: (newUsersCount: number[]) => {
+        this.newUsersCount = newUsersCount;
+
+        this.barChartData = {
+          labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+          datasets: [
+            {
+              data: this.newUsersCount,
+              label: 'Number of new users',
+              backgroundColor: 'rgba(54, 162, 235, 0.2)',
+              borderColor: 'rgb(54, 162, 235)',
+              borderWidth: 1
+            }
+          ]
+        };
+      },
+      error: (error) => {
+        console.error('Error loading chart data count', error);
+      }
+    });
   }
 }
--- src/app/admin/admin.service.ts
+++ src/app/admin/admin.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  constructor() { }
+  private baseUrl = environment.apiUrl
+
+  constructor(private http: HttpClient) { }
+
+  getUserCount() {
+    return this.http.get<number>(this.baseUrl + 'AdminDashboard/user-count');
+  }
+
+  getReviewCount() {
+    return this.http.get<number>(this.baseUrl + 'AdminDashboard/review-count');
+  }
+
+  getAudioBookCount() {
+    return this.http.get<number>(this.baseUrl + 'AdminDashboard/audiobook-count');
+  }
+
+  getNewUsersCount() {
+    return this.http.get<number[]>(this.baseUrl + 'AdminDashboard/user-count-chart');
+  }
 }
